30e919000bdbca10db410b9e0de5e9b1e4fd83d1
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-02-24 03:39:43 +0000

    Updated the settings

--- APIproject/settings.py
+++ APIproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!8df4xfjxg9z+h(jkg9njxkd8#91h!o(ysxm1rq)_ax@&g&tq9"
+SECRET_KEY = "*#-26mtoy8y+@5&0*oscrfw#(^#7+muhmjg30dlxc^k9))#(zx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
