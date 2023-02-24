ef85af56979f020aafc809be5c9a41d730f10541
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-02-24 10:00:03 +0000

    Added the edit and delete functionality

--- api/views.py
+++ api/views.py
@@ -19,5 +19,29 @@ def article_list(request):
         serializer = ArticleSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializers.data, status=201)
+            return JsonResponse(serializer.data, status=201)
         return JSONRespose(serializer.errors, status=400)
+    
+@csrf_exempt
+def article_details(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JSONRespose(serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status==204)
